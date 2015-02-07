dc6d4c3b1b2c8e28193d2928f89b47f86a938bbe
markzhan <markzhann@gmail.com>
2015-02-07 16:09:22 +0800

    osname

--- bin/ip2-cli.js
+++ bin/ip2-cli.js
@@ -5,7 +5,7 @@ var ip2 = require('../');
 var program = require('commander');
 
 program
-  .version('1.0.3')
+  .version('1.0.5')
   .option('-v, --version', 'get version number')
   .option('-f, --force', 'overwrite file')
   .option('-l, --local', 'get local ip')
